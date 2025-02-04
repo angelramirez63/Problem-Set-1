3fe9168b24b321ed2c05348667d6172a45c41575
angelramirez63 <ramirezangel6315@gmail.com>
2025-02-04 18:12:21 -0500

    Update script.r

--- scripts/script.r
+++ scripts/script.r
@@ -6,31 +6,50 @@
 
 #### PUNTO 2 ###
 
-#Limpiar
+  #Limpiar
 rm(list = ls())
 cat("\014")
 
-#Cargar paquetes
+  #Cargar paquetes
 require(pacman)
 p_load(rio, tidyverse, skimr, here, gridExtra, corrplot, stargazer, MASS, rvest)
 
-#Directorio
+  #Directorio
 wd<-here()
 setwd(wd)
 rm(wd)
 
-#Importar base
+  #Importar base
 db <- readRDS("stores/datos_GEIH.rds") %>% 
   as_tibble()
 
-#Limpiar a población objetivo
+  #Limpiar a población objetivo
 db<- db %>% 
   filter(age > 18 & dsi==0)
 
-#Editar variable categórica para género
+  #Editar variable categórica para género, 1 sea igual a mujer
 db<- db %>% 
   mutate(gender=ifelse(sex==1,0,1))
 
+  #Imputar valores faltantes de variables categóricas de interés
+  # Calculando la moda. Para maxEducLevel
+mode_edu <- as.numeric(names(sort(table(db$maxEducLevel), decreasing = TRUE)[1]))
+
+  # Imputar el valor faltante. 
+db <- db  %>%
+  mutate(maxEducLevel = ifelse(is.na(maxEducLevel) == TRUE, mode_edu , maxEducLevel))
+
+  # Para clase, 1 si vive en zona urbana
+mode_clas <- as.numeric(names(sort(table(db$clase), decreasing = TRUE)[1]))
+
+db <- db  %>%
+  mutate(clase = ifelse(is.na(clase) == TRUE, mode_clas , clase))
+
+  # Para formal, 1 si el trabajador tiene un empleo formal
+mode_for <- as.numeric(names(sort(table(db$formal), decreasing = TRUE)[1]))
+
+db <- db  %>%
+  mutate(formal = ifelse(is.na(formal) == TRUE, mode_for , formal))
 
 #### PUNTO 3 ###
 
