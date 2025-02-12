d925419d60afaab1bb24f0870de088e91065d978
angelramirez63 <ramirezangel6315@gmail.com>
2025-02-12 16:56:15 -0500

    Update script.r

--- scripts/script.r
+++ scripts/script.r
@@ -34,6 +34,100 @@ missing_tab<-data.frame(
   )
 missing_tab
 
+  # Eliminar variables donde todos los valores son NA
+db <- db %>%
+  select_if(~ !all(is.na(.)))
+
+####Se imputarán las variables categóricas agrupados por si el individuo está empleado y por su estrato###
+
+    # Función para calcular la moda (el valor más frecuente)
+calcular_moda <- function(x) {
+  tabla <- table(x)
+  moda <- names(tabla)[which.max(tabla)]
+  return(moda)
+}
+
+    #Se reemplazan los valores de las variables categóricas por la moda agrupada si está empleado y su estrado
+db_limpio <- db %>%
+  group_by(dsi, estrato1) %>%
+  mutate(
+    p6090 = ifelse(is.na(p6090) | p6090 == 9, as.numeric(calcular_moda(p6090)), p6090),
+    p6100 = ifelse(is.na(p6100) | p6100 == 9, as.numeric(calcular_moda(p6100)), p6100),
+    p6210 = ifelse(is.na(p6210) | p6210 == 9, as.numeric(calcular_moda(p6210)), p6210),
+    p6510 = ifelse(is.na(p6510) | p6510 == 9, as.numeric(calcular_moda(p6510)), p6510),
+    p6545 = ifelse(is.na(p6545) | p6545 == 9, as.numeric(calcular_moda(p6545)), p6545),
+    p6580 = ifelse(is.na(p6580) | p6580 == 9, as.numeric(calcular_moda(p6580)), p6580),
+    p6585s1 = ifelse(is.na(p6585s1) | p6585s1 == 9, as.numeric(calcular_moda(p6585s1)), p6585s1),
+    p6585s2 = ifelse(is.na(p6585s2) | p6585s2 == 9, as.numeric(calcular_moda(p6585s2)), p6585s2),
+    p6585s3 = ifelse(is.na(p6585s3) | p6585s3 == 9, as.numeric(calcular_moda(p6585s3)), p6585s3),
+    p6585s4 = ifelse(is.na(p6585s4) | p6585s4 == 9, as.numeric(calcular_moda(p6585s4)), p6585s4),
+    p6590 = ifelse(is.na(p6590) | p6590 == 9, as.numeric(calcular_moda(p6590)), p6590),
+    p6600 = ifelse(is.na(p6600) | p6600 == 9, as.numeric(calcular_moda(p6600)), p6600),
+    p6610 = ifelse(is.na(p6610) | p6610 == 9, as.numeric(calcular_moda(p6610)), p6610),
+    p6620 = ifelse(is.na(p6620) | p6620 == 9, as.numeric(calcular_moda(p6620)), p6620),
+    p7500s1 = ifelse(is.na(p7500s1) | p7500s1 == 9, as.numeric(calcular_moda(p7500s1)), p7500s1),
+    p7500s2 = ifelse(is.na(p7500s2) | p7500s2 == 9, as.numeric(calcular_moda(p7500s2)), p7500s2),
+    p7500s3 = ifelse(is.na(p7500s3) | p7500s3 == 9, as.numeric(calcular_moda(p7500s3)), p7500s3),
+    p7510s1 = ifelse(is.na(p7510s1) | p7510s1 == 9, as.numeric(calcular_moda(p7510s1)), p7510s1),
+    p7510s2 = ifelse(is.na(p7510s2) | p7510s2 == 9, as.numeric(calcular_moda(p7510s2)), p7510s2),
+    p7510s3 = ifelse(is.na(p7510s3) | p7510s3 == 9, as.numeric(calcular_moda(p7510s3)), p7510s3),
+    p7510s5 = ifelse(is.na(p7510s5) | p7510s5 == 9, as.numeric(calcular_moda(p7510s5)), p7510s5),
+    p7510s6 = ifelse(is.na(p7510s6) | p7510s6 == 9, as.numeric(calcular_moda(p7510s6)), p7510s6),
+    p7510s7 = ifelse(is.na(p7510s7) | p7510s7 == 9, as.numeric(calcular_moda(p7510s7)), p7510s7)
+  ) %>%
+  ungroup()
+
+    #Ahora realizamos lo mismo, pero con las variables categóricas faltantes
+db_limpio <- db %>%
+  group_by(dsi, estrato1) %>%
+  mutate(
+    p6100 = ifelse(is.na(p6100), calcular_moda(p6100), p6100),
+    oficio = ifelse(is.na(oficio), calcular_moda(oficio), oficio),
+    relab = ifelse(is.na(relab), calcular_moda(relab), relab),
+    p6510s2 = ifelse(is.na(p6510s2), calcular_moda(p6510s2), p6510s2),
+    p6545s2 = ifelse(is.na(p6545s2), calcular_moda(p6545s2), p6545s2),
+    p6580s2 = ifelse(is.na(p6580s2), calcular_moda(p6580s2), p6580s2),
+    p6585s1a2 = ifelse(is.na(p6585s1a2), calcular_moda(p6585s1a2), p6585s1a2),
+    p6585s2a2 = ifelse(is.na(p6585s2a2), calcular_moda(p6585s2a2), p6585s2a2),
+    p6585s3a2 = ifelse(is.na(p6585s3a2), calcular_moda(p6585s3a2), p6585s3a2),
+    p6630s1 = ifelse(is.na(p6630s1), calcular_moda(p6630s1), p6630s1),
+    p6630s2 = ifelse(is.na(p6630s2), calcular_moda(p6630s2), p6630s2),
+    p6630s3 = ifelse(is.na(p6630s3), calcular_moda(p6630s3), p6630s3),
+    p6630s4 = ifelse(is.na(p6630s4), calcular_moda(p6630s4), p6630s4),
+    p6630s6 = ifelse(is.na(p6630s6), calcular_moda(p6630s6), p6630s6),
+    p6870 = ifelse(is.na(p6870), calcular_moda(p6870), p6870),
+    p6920 = ifelse(is.na(p6920), calcular_moda(p6920), p6920),
+    p7040 = ifelse(is.na(p7040), calcular_moda(p7040), p7040),
+    p7050 = ifelse(is.na(p7050), calcular_moda(p7050), p7050),
+    p7090 = ifelse(is.na(p7090), calcular_moda(p7090), p7090),
+    p7110 = ifelse(is.na(p7110), calcular_moda(p7110), p7110),
+    p7120 = ifelse(is.na(p7120), calcular_moda(p7120), p7120),
+    p7140s1 = ifelse(is.na(p7140s1), calcular_moda(p7140s1), p7140s1),
+    p7140s2 = ifelse(is.na(p7140s2), calcular_moda(p7140s2), p7140s2),
+    p7150 = ifelse(is.na(p7150), calcular_moda(p7150), p7150),
+    p7160 = ifelse(is.na(p7160), calcular_moda(p7160), p7160),
+    p7472 = ifelse(is.na(p7472), calcular_moda(p7472), p7472),
+    ina = ifelse(is.na(ina), calcular_moda(ina), ina),
+    cclasnr2 = ifelse(is.na(cclasnr2), calcular_moda(cclasnr2), cclasnr2),
+    cclasnr3 = ifelse(is.na(cclasnr3), calcular_moda(cclasnr3), cclasnr3),
+    cclasnr4 = ifelse(is.na(cclasnr4), calcular_moda(cclasnr4), cclasnr4),
+    cclasnr6 = ifelse(is.na(cclasnr6), calcular_moda(cclasnr6), cclasnr6),
+    cclasnr7 = ifelse(is.na(cclasnr7), calcular_moda(cclasnr7), cclasnr7),
+    cclasnr8 = ifelse(is.na(cclasnr8), calcular_moda(cclasnr8), cclasnr8),
+    cclasnr11 = ifelse(is.na(cclasnr11), calcular_moda(cclasnr11), cclasnr11),
+    maxEducLevel = ifelse(is.na(maxEducLevel), as.numeric(calcular_moda(maxEducLevel)), maxEducLevel),
+    regSalud = ifelse(is.na(regSalud), calcular_moda(regSalud), regSalud),
+    cotPension = ifelse(is.na(cotPension), calcular_moda(cotPension), cotPension),
+    formal = ifelse(is.na(formal), calcular_moda(formal), formal),
+    informal = ifelse(is.na(informal), calcular_moda(informal), informal),
+    microEmpresa = ifelse(is.na(microEmpresa), calcular_moda(microEmpresa), microEmpresa),
+    sizeFirm = ifelse(is.na(sizeFirm), calcular_moda(sizeFirm), sizeFirm)
+  ) %>%
+  ungroup()
+
+
+
+##########################################################################################################################
 
   #Imputar valores faltantes de variables categóricas de interés
   # Por el momento tenemos edad, máximo nivel educativo, formalidad del empleo, ingreso por horas extra, ingreso total, zona urbana o rural, y total de horas trabajadas la semana previa
