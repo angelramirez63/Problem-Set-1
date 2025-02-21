03a785ea93c44590300a21b4261b6fb784b28e4b
Natalia Plata Ángel <n.plata@uniandes.edu.co>
2025-02-21 14:54:34 -0500

    estadísticas descriptivas

---
+++ scripts/Descripción de datos.R
@@ -0,0 +1,42 @@
+# Estadísticas descriptivas ----------------------------------------------------
+rm(list = ls())
+
+## 0. Preparación --------------------------------------------------------------
+
+### Librerías 
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+
+library(pacman)
+
+p_load(tidyverse, rvest, rebus, htmltools, rio, skimr,
+       visdat, margins, stargazer, here, VIM, caret, dplyr)
+
+
+### Crear el directorio 
+wd <- here()
+setwd(wd)
+rm(wd)
+
+## Cargar datos
+db <- readRDS("stores/datos_modelos.rds") %>% 
+  as_tibble()
+
+## Estadísticas descriptivas ---------------------------------------------------
+#Visualización: de las distribuciones de las variables de ingreso 
+
+variables <- c("y_salary_m", "y_salary_m_hu", "y_ingLab_m", "y_ingLab_m_ha", "y_total_m", "y_total_m_ha") 
+
+
+for (variable in variables) {
+  
+  plot <- ggplot(db_limpia, aes_string(variable)) +
+    geom_histogram(color = "#000000", fill = "#0099F8") +
+    geom_vline(xintercept = median(db_limpia[[variable]], na.rm = TRUE), linetype = "dashed", color = "red") +
+    geom_vline(xintercept = mean(db_limpia[[variable]], na.rm = TRUE), linetype = "dashed", color = "blue") +  
+    ggtitle(paste("Distribución", as.character(variable), sep = " ")) +
+    theme_classic() +
+    theme(plot.title = element_text(size = 18))
+  
+  print(plot)
+  
+}
